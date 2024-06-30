633fdd930f4cc093bc1ec56cdaca2b45fb551b03
maplumridge <map205@cam.ac.uk>
2024-06-30 22:24:11 +0100

    Corrected simulations script batch script.

--- simulations.sh
+++ simulations.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-#SBATCH --gres=gpu:3               
+#SBATCH --gres=gpu:1              
 #SBATCH --partition=orchid          
 #SBATCH --account=orchid
 #SBATCH -o %j.out                   
 #SBATCH -e %j.err                   
-#SBATCH --time=3:00:00             
+#SBATCH --time=0:30:00             
 #SBATCH --ntasks=1                  
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=32000
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=64000
 
 ## executables
 conda activate decentralizedEO
 nvidia-smi
 
-python test_main.py
+cd licos
+echo "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
+export CUDA_VISIBLE_DEVICES=0,1,2
+echo "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
+mpiexec --oversubscribe -n 2 python test_main.py
+nvidia-smi > gpu_usage_$SLURM_JOB_ID.log
