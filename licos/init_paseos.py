2007eef5335973ef6085f764dd23269af6b599cb
rasmusmarak <rasmusmarak@gmail.com>
2024-07-15 18:36:08 +0000

    removed more code dupication in mission scenarios.

--- licos/init_paseos.py
+++ licos/init_paseos.py
@@ -41,57 +41,16 @@ def init_paseos(rank, N_ranks):
     ActorBuilder.set_orbit(
         actor=local_actor, position=pos, velocity=v, epoch=t0, central_body=earth
     )
-    # ActorBuilder.add_comm_device(
-    #     actor=local_actor, device_name="Link1", bandwidth_in_kbps=1000
-    # )
 
-    # Battery from https://sentinels.copernicus.eu/documents/247904/349490/S2_SP-1322_2.pdf
-    # 87Ah * 28 Volt = 8.7696e9Ws
-    ActorBuilder.set_power_devices(
-        actor=local_actor,
-        battery_level_in_Ws=277200 * 0.5,
-        max_battery_level_in_Ws=277200,
-        charging_rate_in_W=20,
-    )
+    # Add devices and parameters for physical simulation
+    get_S2_satellite_scaffold(local_actor)
 
-    # TODO update and sanity check
-    ActorBuilder.set_thermal_model(
-        actor=local_actor,
-        actor_mass=6.0,
-        actor_initial_temperature_in_K=283.15,
-        actor_sun_absorptance=0.9,
-        actor_infrared_absorptance=0.5,
-        actor_sun_facing_area=0.012,
-        actor_central_body_facing_area=0.01,
-        actor_emissive_area=0.1,
-        actor_thermal_capacity=6000,
-    )
-
-    cfg = paseos.load_default_cfg()  # loading cfg to modify defaults
-    cfg.sim.start_time = t0.mjd2000 * pk.DAY2SEC  # convert epoch to seconds
-    paseos_instance = paseos.init_sim(local_actor=local_actor, cfg=cfg)
+    # Initialize paseos instance
+    paseos_instance = initialize_paseos_instance(t0, local_actor)
     print(f"Rank {rank} set up its PASEOS instance for its local actor {local_actor}")
 
-    # Ground stations
-    stations = [
-        ["Maspalomas", 27.7629, -15.6338, 205.1],
-        ["Matera", 40.6486, 16.7046, 536.9],
-        ["Svalbard", 78.9067, 11.8883, 474.0],
-    ]
-    groundstation_actors = []
-    for station in stations:
-        gs_actor = ActorBuilder.get_actor_scaffold(
-            name=station[0], actor_type=GroundstationActor, epoch=t0
-        )
-        ActorBuilder.set_ground_station_location(
-            gs_actor,
-            latitude=station[1],
-            longitude=station[2],
-            elevation=station[3],
-            minimum_altitude_angle=5.0,
-        )
-        # paseos_instance.add_known_actor(gs_actor)
-        groundstation_actors.append(gs_actor)
+    # Define groundstations
+    groundstation_actors = get_groundstations(t0)
 
     return (paseos_instance, local_actor, groundstation_actors)
 
@@ -140,9 +99,7 @@ def init_paseos_scenario_sentinel2_with_fl(rank, N_ranks):
     get_S2_satellite_scaffold(local_actor)
 
     # Initialize paseos instance
-    cfg = paseos.load_default_cfg()  # loading cfg to modify defaults
-    cfg.sim.start_time = t0.mjd2000 * pk.DAY2SEC  # convert epoch to seconds
-    paseos_instance = paseos.init_sim(local_actor=local_actor, cfg=cfg)
+    paseos_instance = initialize_paseos_instance(t0, local_actor)
     print(f"Rank {rank} set up its PASEOS instance for its local actor {local_actor}")
 
     # Define groundstations and disaster site
@@ -204,9 +161,7 @@ def init_paseos_scenario_walker_constellation_with_fl(rank, N_ranks):
     get_S2_satellite_scaffold(local_actor)
 
     # Initialize paseos instance
-    cfg = paseos.load_default_cfg()  # loading cfg to modify defaults
-    cfg.sim.start_time = t0.mjd2000 * pk.DAY2SEC  # convert epoch to seconds
-    paseos_instance = paseos.init_sim(local_actor=local_actor, cfg=cfg)
+    paseos_instance = initialize_paseos_instance(t0, local_actor)
     print(f"Rank {rank} set up its PASEOS instance for its local actor {local_actor}")
 
     # Define groundstations and disaster site
@@ -215,7 +170,6 @@ def init_paseos_scenario_walker_constellation_with_fl(rank, N_ranks):
     groundstation_actors.append(disaster_site[0]) 
 
     return (paseos_instance, local_actor, groundstation_actors)
-
 
 
 def init_paseos_scenario_low_altitude_constellation_with_fl_and_relay(rank, N_ranks):
@@ -273,9 +227,7 @@ def init_paseos_scenario_low_altitude_constellation_with_fl_and_relay(rank, N_ra
     get_S2_satellite_scaffold(local_actor)
 
     # Initialize paseos instance
-    cfg = paseos.load_default_cfg()  # loading cfg to modify defaults
-    cfg.sim.start_time = t0.mjd2000 * pk.DAY2SEC  # convert epoch to seconds
-    paseos_instance = paseos.init_sim(local_actor=local_actor, cfg=cfg)
+    paseos_instance = initialize_paseos_instance(t0, local_actor)
     print(f"Rank {rank} set up its PASEOS instance for its local actor {local_actor}")
 
     # Define ground stations and disaster site
@@ -409,3 +361,19 @@ def get_disaster_site(t0):
         )
         disaster_site_actors.append(gs_actor)
     return disaster_site_actors
+
+
+def initialize_paseos_instance(t0, local_actor):
+    """Loading default cfg and start paseos simulation.
+
+    Args:
+        t0 (pk.epoch): Initial time
+        local_actor (SpacecraftActor): The actor to simulate.
+
+    Returns:
+        paseos_instance (paseos): A paseos instance for the local actor.
+    """
+    # Initialize paseos instance
+    cfg = paseos.load_default_cfg()  # loading cfg to modify defaults
+    cfg.sim.start_time = t0.mjd2000 * pk.DAY2SEC  # convert epoch to seconds
+    return paseos.init_sim(local_actor=local_actor, cfg=cfg)
